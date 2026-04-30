a23f9a426967e9e1e31957b10f7c0a458dc37ac7
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-04-30 20:46:48 +0530

    feat: adds a delete function and getMovies function

--- go-crud/main.go
+++ go-crud/main.go
@@ -4,6 +4,7 @@ import(
 	"fmt"
 	"log"
 	"math/rand"
+	"encoding/json"
 	"net/http"
 	"strconv"
 	"github.com/gorilla/mux"
@@ -22,6 +23,21 @@ type Director struct{
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	for index,item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -31,6 +47,7 @@ func main() {
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
